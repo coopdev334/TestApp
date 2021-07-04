07db8ddca297dbe431598b902723a0bb3db9f375
Matthew-J-M <mjm227@njit.edu>
2021-07-04 19:50:10 -0400

    Matthew's Third Commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.testapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
-const val COMPILE_TIME_CONSTANT: String = "Mike change 2"
+const val COMPILE_TIME_CONSTANT: String = "Mike change 2" ///CHANGE
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding  // Use view binding to access views
